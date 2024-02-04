1df622f8c420965250c3968ba9bba2d85b823c76
Raquel Hilburn <141982290+RockyHilburn@users.noreply.github.com>
2024-02-03 19:35:59 -0800

    finishing up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import { Routes, Route, Link } from "react-router-dom";
 import Red from './components/Red';
 import Blue from './components/Blue';
+import Green from './components/Green';
+import Yellow from './components/Yellow';
 import Home from './components/Home';
 
 function App() {
@@ -12,14 +13,18 @@ function App() {
       <div id="container">
         <h1></h1>
         <div id="navbar">
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
+          <Link to="/blue">BLUE</Link>
+          <Link to="/red">RED</Link>
+          <Link to="/green">GREEN</Link>
+          <Link to="/yellow">YELLOW</Link>
           <Link to="/"></Link>
         </div>
         <div id="main-section">
           <Routes>
             <Route path="/blue" element={<Blue />} />
             <Route path="/red" element={<Red />} />
+            <Route path="/green" element={<Green />} />
+            <Route path="/yellow" element={<Yellow />} />
             <Route path="/" element={<Home />}/>
           </Routes>
         </div>
